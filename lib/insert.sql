92094061f28c866fb1f5fdf6fdda3846a5378362
Jamal Turner <jlsmitty2273@yahoo.com>
2019-11-26 14:04:03 -0500

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,30 @@
 INSERT INTO series(id, title, author_id, subgenre_id) VALUES (1, "One Punch Man", 1, 1);
 INSERT INTO series(id, title, author_id, subgenre_id) VALUES (2, "Redman", 2, 2);
+INSERT INTO books(id, title, year, series_id) VALUES (1, "One punch begin", 2011, 1);
+INSERT INTO books(id, title, year, series_id) VALUES (2, "One punch middle", 2012, 1);
+INSERT INTO books(id, title, year, series_id) VALUES (3, "One punch near end", 2013, 2);
+INSERT INTO books(id, title, year, series_id) VALUES (4, "One punch end", 2014, 2);
+INSERT INTO books(id, title, year, series_id) VALUES (5, "RED !", 2011, 2);
+INSERT INTO books(id, title, year, series_id) VALUES (6, "RED20", 2011, 2);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (1, "john", "wing it", "human", 1);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (2, "jon", "wing him", "human", 1);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (3, "joh", "it here", "human", 1);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (4, "ron", "eat it", "snake", 1);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (5, "Sait", "  ", "human", 1);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (6, "REDMAN", "bruh", "human", 2);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (7, "Cop", "beat it", "human", 2);
+INSERT INTO characters(id, name, motto, species, author_id) VALUES (8, "hoe", "money", "trick", 2);
+INSERT INTO subgenres (id, name) VALUES (1, "horror");
+INSERT INTO subgenres (id, name) VALUES (2, "comedy");
+INSERT INTO authors (id, name) VALUES (1, "Tokyo");
+INSERT INTO authors (id, name) VALUES (2, "Redman");
+INSERT INTO character_books(id, character_id, book_id) VALUES 
+INSERT INTO character_books(id, character_id, book_id) VALUES 
+
+
+
+
+
+
+
+
